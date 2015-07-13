ec32bcf3ef4179aa21ddd91fe905c6f2b351db14
Komal Rathi <komalsrathi@users.noreply.github.com>
2015-07-13 15:27:33 -0400

    Create annotate_eset.R

---
+++ annotate_eset.R
@@ -0,0 +1,11 @@
+annotate.eset <- function(eset, transcriptdb, ensemblonly=T){
+  gns <- AnnotationDbi::select(transcriptdb, keys=featureNames(eset), columns=c("SYMBOL","GENENAME",'ENTREZID','ENSEMBL'), keytype="PROBEID")
+  gns_cts <- dplyr::count(gns,PROBEID)
+  colnames(gns_cts)[2] <- "GenestoProbe"
+  gns <- gns[!duplicated(gns[,1]),]
+  gnsmerg <- dplyr::inner_join(gns,gns_cts,by='PROBEID')
+  colnames(gnsmerg)[1] ='ID'
+  gnsmerg = gnsmerg[,-6]
+  fData(eset) <- gnsmerg
+  return(eset)
+}
