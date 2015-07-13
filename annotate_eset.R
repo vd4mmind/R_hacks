ca11c81c3089abc77453999f77e1523a610b9bdf
Komal Rathi <komalsrathi@users.noreply.github.com>
2015-07-13 15:28:14 -0400

    Update annotate_eset.R

--- annotate_eset.R
+++ annotate_eset.R
@@ -1,5 +1,6 @@
 annotate.eset <- function(eset, transcriptdb, ensemblonly=T){
-  gns <- AnnotationDbi::select(transcriptdb, keys=featureNames(eset), columns=c("SYMBOL","GENENAME",'ENTREZID','ENSEMBL'), keytype="PROBEID")
+  gns <- AnnotationDbi::select(transcriptdb, keys=featureNames(eset), columns=c("SYMBOL","GENENAME",'ENTREZID','ENSEMBL'), 
+                                keytype="PROBEID")
   gns_cts <- dplyr::count(gns,PROBEID)
   colnames(gns_cts)[2] <- "GenestoProbe"
   gns <- gns[!duplicated(gns[,1]),]
